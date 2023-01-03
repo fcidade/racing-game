301cae006ac05c75deeeed85abc1f61429c730e6
Francisco Cidade <franciscocidadehomemjunior@gmail.com>
2023-01-03 18:11:13 -0300

    Nice camera :)

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -13,18 +13,30 @@ public class CameraFollow : MonoBehaviour
     [SerializeField]
     float cameraVelocity = 3f;
 
+    [SerializeField, Range(0f, 1f)]
+    float cameraRotationVelocity = .5f;
+
     [SerializeField]
     float cameraHeight = 10f;
 
-    void Update()
+    new Camera camera;
+
+    void Awake() {
+        camera = GetComponentInChildren<Camera>();
+    }
+
+    void FixedUpdate()
     {
-        if (!target) return;
-        transform.LookAt(target);
+        if (!target || !camera) return;
+        // transform.LookAt(target);
+        camera.transform.LookAt(target.position);
+        
+        var newCameraPosition = target.position + (-target.forward * cameraDistance) + (target.up * cameraHeight);
+        camera.transform.position = Vector3.Lerp(camera.transform.position, newCameraPosition, Time.deltaTime * cameraRotationVelocity);
 
-        Vector3 newCameraPosition = target.position + (-Vector3.forward * cameraDistance) + (Vector3.up * cameraHeight);
-        transform.position = Vector3.Lerp(transform.position, newCameraPosition, Time.deltaTime * cameraVelocity);
+        transform.position = Vector3.Lerp(transform.position, target.position, Time.deltaTime * cameraVelocity);
 
-        // transform.rotation = Quaternion.Lerp(transform.rotation, target.rotation, Time.deltaTime);
+        // transform.rotation = Quaternion.Lerp(transform.rotation, target.rotation, Time.deltaTime * cameraRotationVelocity);
         
         // transform.RotateAround(target.position, Vector3.up, cameraAngle);
     }
