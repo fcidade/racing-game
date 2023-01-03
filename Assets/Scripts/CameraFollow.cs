ba3369f7716546a3c8eff7038950abb825e5a80e
Francisco Cidade <franciscocidadehomemjunior@gmail.com>
2023-01-03 17:54:20 -0300

    Making a better camera

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -10,13 +10,22 @@ public class CameraFollow : MonoBehaviour
     [SerializeField]
     float cameraDistance = 2f;
 
+    [SerializeField]
+    float cameraVelocity = 3f;
+
+    [SerializeField]
+    float cameraHeight = 10f;
+
     void Update()
     {
         if (!target) return;
         transform.LookAt(target);
-        if (Vector3.Distance(transform.position, target.position) > cameraDistance)
-        {
-            transform.position = Vector3.Lerp(transform.position, target.position + (-target.forward * cameraDistance), Time.deltaTime);
-        }
+
+        Vector3 newCameraPosition = target.position + (-Vector3.forward * cameraDistance) + (Vector3.up * cameraHeight);
+        transform.position = Vector3.Lerp(transform.position, newCameraPosition, Time.deltaTime * cameraVelocity);
+
+        // transform.rotation = Quaternion.Lerp(transform.rotation, target.rotation, Time.deltaTime);
+        
+        // transform.RotateAround(target.position, Vector3.up, cameraAngle);
     }
 }
